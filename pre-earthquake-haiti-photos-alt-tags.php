d47c1b06c4d1becb1a915375e84a53b4c3db9b17
Guy Radcliffe <gr7432@gmail.com>
2012-09-26 11:13:57 -0400

    add alt tag file

---
+++ pre-earthquake-haiti-photos-alt-tags.php
@@ -0,0 +1,32 @@
+<?php
+$alttagsarray = array(
+'',
+'',
+'',
+'Ginette Sylvain on her wedding day December 2002.',
+'Ginette Sylvain at Champs de Mars next to a tap tap.',
+'Killing a pig',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'From the Balcony of Detente Tropical Hotel in La Plaine, Haiti',
+'Killing a pig',
+'Killing a pig',
+'Killing a pig',
+'A friend of the Sylvain Family from La Plain, Haiti.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+'The busy streets of Port-au-Prince on a Saturday afternoon in 2003.',
+);
+
+?>
